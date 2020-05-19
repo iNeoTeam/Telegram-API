2443c5b80f67b8ba64f7ac3f52d778b208be35a8
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2020-05-19 13:06:05 +0430

    api.php

--- api.php
+++ api.php
@@ -1,6 +1,7 @@
 <?php
 /*
 IN THE NAME OF ALLAH
+
 Sample Name			:	Telegram API without Filtering
 Coder				:	Amir Hossein Yeganeh [Sir.4m1R]
 Coder Telegram		:	@Sir4m1R
@@ -8,6 +9,7 @@ GitHub Address		:	https://github.com/iNeoTeam/Telegram-API
 Telegram Channel	:	@iNeoTeam
 */
 error_reporting(0);
+header("content-type: application/json; charset: UTF-8");
 $token = $_GET['token'];
 $method = $_GET['method'];
 
