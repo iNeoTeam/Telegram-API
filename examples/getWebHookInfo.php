3b6dcc9802ca2c2da9ee46244867326b6e832cd4
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2020-05-19 13:13:12 +0430

    getWebHookInfo.php

---
+++ examples/getWebHookInfo.php
@@ -0,0 +1,28 @@
+<?php
+/*
+IN THE NAME OF ALLAH
+
+Sample Name			:	Telegram API without Filtering
+Coder				:	Amir Hossein Yeganeh [Sir.4m1R]
+Coder Telegram		:	@Sir4m1R
+GitHub Address		:	https://github.com/iNeoTeam/Telegram-API
+Telegram Channel	:	@iNeoTeam
+*/
+error_reporting(0);
+header("content-type: application/json; charset: UTF-8");
+$token			= "YOUR BOT TOKEN";
+$method			= "getWebHookInfo";
+
+function telegram($token, $method){
+	$api = "https://api.ineo-team.ir"; //don't change it !
+	$telegram = $api."/telegram.php?token=".$token."&method=".strtolower($method);
+	$data = file_get_contents($telegram);
+	return $data;
+}
+
+echo telegram($token, $method);
+
+if(file_exists("error_log")){
+	unlink("error_log");
+}
+?>
