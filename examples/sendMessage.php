763d925f9d23edcfb10e6b303473efcb1c784d52
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2020-05-19 13:13:30 +0430

    sendMessage.php

---
+++ examples/sendMessage.php
@@ -0,0 +1,32 @@
+<?php
+/*
+IN THE NAME OF ALLAH
+
+Sample Name			:	Telegram API without Filtering
+Coder				:	Amir Hossein Yeganeh [Sir.4m1R]
+Coder Telegram		:	@Sir4m1R
+GitHub Address		:	https://github.com/iNeoTeam/Telegram-API
+Telegram Channel	:	@iNeoTeam
+*/
+error_reporting(0);
+header("content-type: application/json; charset: UTF-8");
+$token			= "YOUR BOT TOKEN";
+$method			= "sendMessage";
+$chat_id		= "USER/CHAT/CHANNEL/GROUP/SUPERGROUP ID";
+$message		= "YOUR TEXT MESSAGE";
+$parameters		= "chat_id=".$chat_id."&text=".$message;
+
+function telegram($token, $method, $parameters){
+	$api = "https://api.ineo-team.ir"; //don't change it !
+	$_parameters = urlencode($parameters);
+	$telegram = $api."/telegram.php?token=".$token."&method=".strtolower($method)."&parameters=".$_parameters;
+	$data = file_get_contents($telegram);
+	return $data;
+}
+
+echo telegram($token, $method, $parameters);
+
+if(file_exists("error_log")){
+	unlink("error_log");
+}
+?>
